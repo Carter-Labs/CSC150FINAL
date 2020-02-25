40dcaf10a5ea17e993b95b2f69f1adf3fc608613
Carter Cobb <carter.cobb72@gmail.com>
2020-02-25 11:27:06 -0700

    Added JavaDocs

--- dev.carterlabs.csc150.final.src/model/objects/Currency.java
+++ dev.carterlabs.csc150.final.src/model/objects/Currency.java
@@ -1,24 +1,30 @@
 package model.objects;
 
 public class Currency {
-    /*
+    /**
      * Variables
      */
     private int value;
 
-    /*
-     * Constructors
+    /**
+     * Currency constructor
+     * @param value Currency value
      */
     public Currency(int value){
         this.setValue(value);
     }
 
-    /*
-     * Getters and Setters
+    /**
+     * @return Currency value
      */
     public int getValue() {
         return value;
     }
+
+    /**
+     * Sets currency value
+     * @param value Currency value
+     */
     public void setValue(int value) {
         if(value < 0){
             throw new IllegalArgumentException("value must be greater than 0.");
@@ -26,8 +32,8 @@ public class Currency {
         this.value = value;
     }
 
-    /*
-     * To String
+    /**
+     * @return Currency description
      */
     @Override public String toString() {
         return "model.objects.Currency{" +
