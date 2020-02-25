90aad4c22c00a8a24b15dec5d285b931e7f6f567
Carter Cobb <carter.cobb72@gmail.com>
2020-02-25 11:21:00 -0700

    Added JavaDocs

--- dev.carterlabs.csc150.final.src/model/objects/Weapon.java
+++ dev.carterlabs.csc150.final.src/model/objects/Weapon.java
@@ -3,42 +3,59 @@ package model.objects;
 import java.util.Objects;
 
 public class Weapon {
-    /*
+    /**
      * Variables
      */
     private int damage;
     private WeaponType weaponType;
 
-    /*
-     * Constructors
+    /**
+     * Weapon Constructor
+     * @param damage Weapon damage
+     * @param weaponType Weapon type
      */
-    public Weapon() {}
     public Weapon(int damage, WeaponType weaponType) {
         this.setDamage(damage);
         this.setWeaponType(weaponType);
     }
 
-    /*
-     * Getters and setters
+    /**
+     * @return Weapon Damage
      */
     public int getDamage() {
         return damage;
     }
+
+    /**
+     * Sets the weapon damage
+     * @param damage Weapon damage
+     */
     public void setDamage(int damage) {
         if(damage < 0) {
             throw new IllegalArgumentException("damage must be greater than 0.");
         }
         this.damage = damage;
     }
+
+    /**
+     * @return Weapon type
+     */
     public WeaponType getWeaponType() {
         return weaponType;
     }
+
+    /**
+     * Sets the weapon type
+     * @param weaponType Weapon type
+     */
     public void setWeaponType(WeaponType weaponType) {
         this.weaponType = weaponType;
     }
 
-    /*
-     * Equal and Hash
+    /**
+     * Compares weapons
+     * @param o Weapon object
+     * @return If the weapons are equal
      */
     @Override public boolean equals(Object o) {
         if (this == o) return true;
@@ -47,12 +64,16 @@ public class Weapon {
         return damage == weapon.damage &&
                 weaponType == weapon.weaponType;
     }
+
+    /**
+     * @return Hash of weapons
+     */
     @Override public int hashCode() {
         return Objects.hash(damage, weaponType);
     }
 
-    /*
-     * To String
+    /**
+     * @return Weapon description
      */
     @Override public String toString() {
         return "model.objects.Weapon{" +
