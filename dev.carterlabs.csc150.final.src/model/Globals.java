54ede6d79e785251f2dc3a0d285464acfdeefef2
Carter Cobb <carter.cobb72@gmail.com>
2020-02-25 11:17:19 -0700

    Added JavaDocs

--- dev.carterlabs.csc150.final.src/model/Globals.java
+++ dev.carterlabs.csc150.final.src/model/Globals.java
@@ -5,13 +5,13 @@ import model.objects.WeaponType;
 import java.util.Random;
 
 public class Globals {
-    /*
+    /**
      * Generals
      */
     public static Random rand = new Random();
 
-    /*
-     * ShopController.java
+    /**
+     * ShopController.java used variables
      */
     public static double playerReloadSpeed;
     public static int playerProjectileCount;
@@ -20,27 +20,33 @@ public class Globals {
     public static int playerHealth;
     public static int playerSpeed;
 
-    /*
-     * UIController.java
+    /**
+     * UIController.java used variables
      */
     public static int amountOfCurrency;
 
-    /*
-     * Gun Selection
+    /**
+     * Gun Selection variables
      */
     public static WeaponType weaponType;
 
-    /*
+    /**
      * Info for Chamber.java
      */
     public static double inintalBossSpanPerc = 0.05;
     public static boolean isBossInChamber;
     public static int maxNumOfOfficersAndGuards = 5;
 
+    /**
+     * Loads all the saved variables in this file and set them directly
+     */
     public static void loadData()  {
         //load saved data
     }
 
+    /**
+     * Saves all the variables in this file to the save location
+     */
     public static void saveData() {
         //Saves every value in this file except the generals
     }
