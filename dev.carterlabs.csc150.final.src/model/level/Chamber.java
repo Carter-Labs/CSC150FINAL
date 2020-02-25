c2d46e6dd0ae40a08e600d754bb7be7911945f83
CarterWilde <carterjwilde@gmail.com>
2020-02-24 18:12:38 -0700

    Added Door Generation

--- dev.carterlabs.csc150.final.src/model/level/Chamber.java
+++ dev.carterlabs.csc150.final.src/model/level/Chamber.java
@@ -16,9 +16,11 @@ public class Chamber implements Generate {
      */
     private GameObject[] objects;
     private List<Entity> entities = new ArrayList<>();
+    private ChamberDoorOptions[] doors;
 
     public Chamber(){
         this.generate();
+        doors = new ChamberDoorOptions[4];
     }
 
     /*
@@ -26,9 +28,52 @@ public class Chamber implements Generate {
      */
     @Override public void generate() {
         //add boss if in range to list of entities
+        generateDoors();
         isBossInChamber();
         //add num of enemies to list of entities
 
+    }
+
+    private void generateDoors() {
+        int doors = Globals.rand.nextInt(4);
+        for (int i = 0; i < doors; i++) {
+            int dir = Globals.rand.nextInt(4);
+            switch (dir) {
+                case 0:
+                    if(!addDoor(ChamberDoorOptions.NORTH, i)) --i;
+                    break;
+                case 1:
+                    if(!addDoor(ChamberDoorOptions.EAST, i)) --i;
+                    break;
+                case 2:
+                    if(!addDoor(ChamberDoorOptions.SOUTH, i)) --i;
+                    break;
+                case 3:
+                    if(!addDoor(ChamberDoorOptions.WEST, i)) --i;
+                    break;
+            }
+        }
+    }
+
+    /**
+     * @param option What option you want to add
+     * @param i The index to set it to
+     * @return If it was able to add to the array
+     */
+    private boolean addDoor(ChamberDoorOptions option, int i) {
+        if(!contained(option, doors)){
+            doors[i] = option;
+            return  true;
+        } else {
+            return  false;
+        }
+    }
+
+    private boolean contained(ChamberDoorOptions option, ChamberDoorOptions[] array) {
+        for (ChamberDoorOptions opt: array) {
+            return opt.equals(option);
+        }
+        return false;
     }
 
     public void draw() {
