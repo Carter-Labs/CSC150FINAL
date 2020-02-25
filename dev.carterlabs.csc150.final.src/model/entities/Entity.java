a80ed81b9568e63268cc92604fd2ca8eea03764a
Carter Cobb <carter.cobb72@gmail.com>
2020-02-25 11:13:26 -0700

    Added JavaDocs

--- dev.carterlabs.csc150.final.src/model/entities/Entity.java
+++ dev.carterlabs.csc150.final.src/model/entities/Entity.java
@@ -5,60 +5,87 @@ import model.objects.Weapon;
 import java.util.Objects;
 
 public class Entity {
-    /*
+    /**
      * Variables
      */
     private int health;
     private double speed;
     private Weapon weapon;
 
-    /*
-     * Constructors
+    /**
+     * Constructor of the entity
+     * @param health Heath of the entity
+     * @param speed Speed of the entity
+     * @param weapon Weapon of the entity
      */
-    public Entity(){}
     public Entity(int health,int speed, Weapon weapon) {
         this.setHealth(health);
         this.setSpeed(speed);
         this.setWeapon(weapon);
     }
 
-    /*
-     * Getters and Setters
+    /**
+     * @return entity health
      */
     public int getHealth() {
         return health;
     }
+
+    /**
+     * Sets the entity health
+     * @param health Entity health
+     */
     public void setHealth(int health) {
         if(health < 0 || health > 300) {
             throw new IllegalArgumentException("health must be between 0 and 100.");
         }
         this.health = health;
     }
+
+    /**
+     * @return Entity speed
+     */
     public double getSpeed() {
         return speed;
     }
+
+    /**
+     * Sets the entity speed
+     * @param speed Entity speed
+     */
     public void setSpeed(double speed) {
         if(speed < 0){
             throw new IllegalArgumentException("speed must be greater than 0.");
         }
         this.speed = speed;
     }
+
+    /**
+     * @return Entity weapon
+     */
     public Weapon getWeapon() {
         return weapon;
     }
+
+    /**
+     * Sets the entity weapon
+     * @param weapon Entity weapon
+     */
     public void setWeapon(Weapon weapon) {
         this.weapon = weapon;
     }
 
-    /*
-     * Other methods
+    /**
+     * Movement
      */
     public void movement() {
         //move
     }
 
-    /*
-     * Equals and Hash
+    /**
+     * Compares the entities
+     * @param o Entity to compare
+     * @return If the entities are equal
      */
     @Override public boolean equals(Object o) {
         if (this == o) return true;
@@ -67,12 +94,16 @@ public class Entity {
         return health == entity.health &&
                 Double.compare(entity.speed, speed) == 0;
     }
+
+    /**
+     * @return Hash of the entity
+     */
     @Override public int hashCode() {
         return Objects.hash(health, speed);
     }
 
-    /*
-     * To String
+    /**
+     * @return Entity description
      */
     @Override public String toString() {
         return "model.entities.Entity{" +
