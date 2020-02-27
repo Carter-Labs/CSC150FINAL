c56f7c7c81e5ef3ff225beaa58e36a098a468dc1
CarterWilde <carterjwilde@gmail.com>
2020-02-27 08:41:42 -0700

    Init PlayerController

--- dev.carterlabs.csc150.final.src/controller/PlayerController.java
+++ dev.carterlabs.csc150.final.src/controller/PlayerController.java
@@ -1,4 +1,40 @@
 package controller;
 
-public class PlayerController {
+import model.entities.Player;
+import view.PlayerView;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class PlayerController implements KeyListener {
+    Player player;
+    PlayerView view;
+
+    public PlayerController(Player player) {
+        view = new PlayerView();
+        setPlayer(player);
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
--- dev.carterlabs.csc150.final.src/view/PlayerView.java
+++ dev.carterlabs.csc150.final.src/view/PlayerView.java
@@ -1,27 +1,35 @@
 package view;
 
-import model.events.Started;
 import model.events.Updated;
 
+import javax.swing.*;
 import java.awt.*;
 
-public class PlayerView extends ImageView implements Started, Updated {
+public class PlayerView extends ImageView implements Updated {
     public PlayerView() {
         super.setLayout(null);
-    }
-
-    @Override
-    public void Start() {
+        setBounds(0, 0, 500, 500);
     }
 
     @Override
     public void Update() {
-
+        repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         paintChildren(g);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Player View Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 500, 500);
+        frame.getContentPane().setBackground(Color.RED);
+        frame.setLayout(null);
+        PlayerView view = new PlayerView();
+        frame.add(view);
+        frame.setVisible(true);
     }
 }
